62ea67d33469dd253d0afc363196ebf63e79ee90
Azril <azrilre4@gmail.com>
2021-12-02 14:36:45 +0800

    convert location into city

--- track/views.py
+++ track/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from geopy.geocoders import Nominatim
 import urllib.request 
 import json
 # Create your views here.
@@ -9,12 +10,23 @@ import json
 
 def displayISS(request):
     # url = "http://api.open-notify.org/astros.json"
-    url = "http://api.open-notify.org/iss-now.json" 
+    # url = "http://api.open-notify.org/iss-now.json" 
+    url = "https://api.wheretheiss.at/v1/satellites/25544"
     response = urllib.request.urlopen(url) 
     result = json.loads(response.read())
-    print(result['iss_position'])
-    context = {'result':result}
-    return render(request, 'home.html', context)
+
+    if request.POST:
+        pass
+    else:
+        latitude = str(result["latitude"])
+        longitude = str(result["longitude"])
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        location = geolocator.reverse(latitude+","+longitude)
+        # location = geolocator.reverse("11.001202151377, -125.29176594612")
+        # print(result['iss_position']["longitude"])
+        print(location, latitude, longitude)
+        context = {'result':result}
+        return render(request, 'home.html', context)
 
 @api_view(['GET'])
 def post_collection(request):
